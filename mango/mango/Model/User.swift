51200e830768cef420d5b1091811361f58fb5e10
Jade Thomason <jadethomason@ElMagniiorChode.hsd1.ut.comcast.net>
2018-09-18 21:16:38 -0600

    First Commit

---
+++ mango/mango/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  mango
+//
+//  Created by Jade Thomason on 9/9/18.
+//  Copyright © 2018 Jade Thomason. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    let username: String
+    let firstName: String
+    let lastName: String
+    
+    init(username: String, firstName: String, lastName: String) {
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    
+}
